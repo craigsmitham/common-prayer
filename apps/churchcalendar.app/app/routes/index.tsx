921041852ad8ade89e97e5e5cca132e602576218
Craig Smitham <craigsmitham@outlook.com>
2024-11-20 13:28:35 -0600

    formatting

--- apps/churchcalendar.app/app/routes/index.tsx
+++ apps/churchcalendar.app/app/routes/index.tsx
@@ -1,39 +1,37 @@
 // app/routes/index.tsx
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/start'
+import * as fs from "node:fs";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/start";
 
-const filePath = 'count.txt'
+const filePath = "count.txt";
 
 async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-    )
+  return parseInt(
+    await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
+  );
 }
 
 const getCount = createServerFn({
-    method: 'GET',
+  method: "GET",
 }).handler(() => {
-    return readCount()
-})
+  return readCount();
+});
 
-const updateCount = createServerFn({ method: 'POST' })
-    .validator((d: number) => d)
-    .handler(async ({ data }) => {
-        const count = await readCount()
-        await fs.promises.writeFile(filePath, `${count + data}`)
-    })
+const updateCount = createServerFn({ method: "POST" })
+  .validator((d: number) => d)
+  .handler(async ({ data }) => {
+    const count = await readCount();
+    await fs.promises.writeFile(filePath, `${count + data}`);
+  });
 
-export const Route = createFileRoute('/')({
-    component: Home,
-    loader: async () => await getCount(),
-})
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getCount(),
+});
 
 function Home() {
-    const router = useRouter()
-    const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
-    return (
-        <div>Church Calendar {projectName}</div>
-    )
+  return <div>Church Calendar {}</div>;
 }
--- apps/commonprayer.app/app/routes/index.tsx
+++ apps/commonprayer.app/app/routes/index.tsx
@@ -1,39 +1,38 @@
 // app/routes/index.tsx
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/start'
+import * as fs from "node:fs";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/start";
+import { projectName } from "common-prayer-lib/src/common";
 
-const filePath = 'count.txt'
+const filePath = "count.txt";
 
 async function readCount() {
-    return parseInt(
-        await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-    )
+  return parseInt(
+    await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
+  );
 }
 
 const getCount = createServerFn({
-    method: 'GET',
+  method: "GET",
 }).handler(() => {
-    return readCount()
-})
+  return readCount();
+});
 
-const updateCount = createServerFn({ method: 'POST' })
-    .validator((d: number) => d)
-    .handler(async ({ data }) => {
-        const count = await readCount()
-        await fs.promises.writeFile(filePath, `${count + data}`)
-    })
+const updateCount = createServerFn({ method: "POST" })
+  .validator((d: number) => d)
+  .handler(async ({ data }) => {
+    const count = await readCount();
+    await fs.promises.writeFile(filePath, `${count + data}`);
+  });
 
-export const Route = createFileRoute('/')({
-    component: Home,
-    loader: async () => await getCount(),
-})
+export const Route = createFileRoute("/")({
+  component: Home,
+  loader: async () => await getCount(),
+});
 
 function Home() {
-    const router = useRouter()
-    const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
-    return (
-        <div>Common Prayer</div>
-    )
+  return <div>Common Prayer howdy {projectName}</div>;
 }
