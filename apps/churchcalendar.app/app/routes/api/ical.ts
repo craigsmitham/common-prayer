d01dd3febb68988b719118b2774158fc44ac9493
Craig Smitham <craigsmitham@outlook.com>
2024-11-23 19:58:33 -0600

    add epiphany and days of christmas

--- apps/churchcalendar.app/app/routes/api/ical.ts
+++ apps/churchcalendar.app/app/routes/api/ical.ts
@@ -1,6 +1,8 @@
 import { createAPIFileRoute } from '@tanstack/start/api';
 import { createChurchCalendar } from 'common-prayer-lib/src/calendar/ical';
 
+type CalendarClients = 'google' | 'outlook' | 'outlook-for-web' | 'apple-ios';
+
 export const Route = createAPIFileRoute('/api/ical')({
   GET: ({ request, params }) => {
     const rootUrl = new URL(request.url).origin;
--- packages/common-prayer-lib/src/calendar/ical.ts
+++ packages/common-prayer-lib/src/calendar/ical.ts
@@ -4,12 +4,30 @@ import { Temporal } from 'temporal-polyfill';
 
 type Event = { name: string; description: string };
 
-type Day = {
+const daysOfChristmas = [
+  '1st Day of Christmas',
+  '2nd Day of Christmas',
+  '3rd Day of Christmas',
+  '4th Day of Christmas',
+  '5th Day of Christmas',
+  '6th Day of Christmas',
+  '7th Day of Christmas',
+  '8th Day of Christmas',
+  '9th Day of Christmas',
+  '10th Day of Christmas',
+  '11th Day of Christmas',
+  '12th Day of Christmas',
+] as const;
+type DaysOfChristmas = (typeof daysOfChristmas)[number];
+type Days = 'Christmas' | DaysOfChristmas | 'Epiphany' | 'Easter';
+
+type Day<TDay extends Days> = {
+  name: TDay;
   date: Temporal.PlainDate;
 } & Event;
 
-export function getEasterDay(year: number): Day {
-  const date = getEasterDate(year);
+export function getEasterDay(isoYear: number): Day<'Easter'> {
+  const date = getEasterDate(isoYear);
   return {
     name: 'Easter',
     description: `Easter day ${date.year}`,
@@ -17,7 +35,19 @@ export function getEasterDay(year: number): Day {
   };
 }
 
-export function getChristmasDay({ easter }: { easter: Day }): Day {
+export function getDayOfChristmas(
+  christmas: Day<'Christmas'>,
+): Day<DaysOfChristmas>[] {
+  return daysOfChristmas.map((name, i) => {
+    return {
+      name,
+      description: name,
+      date: christmas.date.add(Temporal.Duration.from({ days: i })),
+    };
+  });
+}
+
+export function getChristmasDay(easter: Day<'Easter'>): Day<'Christmas'> {
   const date = new Temporal.PlainDate(easter.date.year - 1, 12, 25);
   return {
     name: 'Christmas',
@@ -26,10 +56,25 @@ export function getChristmasDay({ easter }: { easter: Day }): Day {
   };
 }
 
-export function getChurchCalendarEvents(isoYear: number): Day[] {
+export function getEpiphanyDay(christmas: Day<'Christmas'>): Day<'Epiphany'> {
+  const date = christmas.date.add(Temporal.Duration.from({ days: 12 }));
+  return {
+    name: 'Epiphany',
+    description: `Epiphany`,
+    date,
+  };
+}
+
+export function getChurchCalendarEvents(isoYear: number): Day<Days>[] {
   const easter = getEasterDay(isoYear);
-  const christmas = getChristmasDay({ easter });
-  return [christmas, easter];
+  const christmas = getChristmasDay(easter);
+
+  return [
+    christmas,
+    ...getDayOfChristmas(christmas),
+    getEpiphanyDay(christmas),
+    easter,
+  ];
 }
 
 export function createChurchCalendar({
