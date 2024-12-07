efc429cfdcda684882509f799fc006a2f11f501e
Craig Smitham <craigsmitham@outlook.com>
2024-12-06 21:22:19 -0600

    updated packaging

--- apps/churchcalendar.app/app/server.ts
+++ apps/churchcalendar.app/app/server.ts
@@ -1,6 +1,5 @@
 import { createHonoServer } from 'react-router-hono-server/node';
 
-console.log('loading server');
 export default await createHonoServer({
   configure: (server) => {
     server.get('/api/test', (c) => {
