ea0363bd4a7df703055d6867c506658c00d11d0e
Craig Smitham <craigsmitham@outlook.com>
2024-12-07 12:38:06 -0600

    added server config

--- apps/churchcalendar.app/app/server.ts
+++ apps/churchcalendar.app/app/server.ts
@@ -1,9 +1,17 @@
 import { createHonoServer } from 'react-router-hono-server/node';
+import { handle } from 'hono/vercel';
+import { Hono } from 'hono';
 
-export default await createHonoServer({
+export const config = {
+  runtime: 'edge',
+};
+
+const app: Hono = await createHonoServer({
   configure: (server) => {
     server.get('/api/test', (c) => {
       return c.text('hello world');
     });
   },
 });
+
+export default handle(app);
