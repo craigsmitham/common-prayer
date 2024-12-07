567220f7bfb4011de9b9c0866784304e4d8c50b2
Craig Smitham <craigsmitham@outlook.com>
2024-12-07 13:20:01 -0600

    removing vercel

--- apps/churchcalendar.app/app/server.ts
+++ apps/churchcalendar.app/app/server.ts
@@ -2,20 +2,10 @@ import { createHonoServer } from 'react-router-hono-server/node';
 import { handle } from 'hono/vercel';
 import { Hono } from 'hono';
 
-export const config = {
-  runtime: 'edge',
-};
-
-const app: Hono = await createHonoServer({
+export default await createHonoServer({
   configure: (server) => {
     server.get('/api/test', (c) => {
       return c.text('hello world');
     });
   },
 });
-
-const app2 = new Hono();
-
-app2.get('/', (c) => c.text('hello world basic'));
-
-export default handle(app2);
