f5f4fa559269faa4fba53635063c08740a536e79
Craig Smitham <craigsmitham@outlook.com>
2024-12-07 13:23:47 -0600

    adding back in vercel handler

--- apps/churchcalendar.app/app/server.ts
+++ apps/churchcalendar.app/app/server.ts
@@ -1,11 +1,12 @@
 import { createHonoServer } from 'react-router-hono-server/node';
 import { handle } from 'hono/vercel';
-import { Hono } from 'hono';
 
-export default await createHonoServer({
+const server = await createHonoServer({
   configure: (server) => {
     server.get('/api/test', (c) => {
       return c.text('hello world');
     });
   },
 });
+
+export default handle(server);
